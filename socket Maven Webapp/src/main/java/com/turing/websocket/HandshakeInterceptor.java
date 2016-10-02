c5667360b482dba47052acab3bc101f2cc190e58
unknown <>
2016-10-02 12:18:49 +0800

    初次提交

---
+++ socket Maven Webapp/src/main/java/com/turing/websocket/HandshakeInterceptor.java
@@ -0,0 +1,31 @@
+package com.turing.websocket;
+
+import java.util.Map;
+
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.web.socket.server.support.HttpSessionHandshakeInterceptor;
+
+/**
+ * websocket握手类
+ * @author cheny
+ *
+ */
+public class HandshakeInterceptor extends HttpSessionHandshakeInterceptor{
+
+	  @Override
+	  public boolean beforeHandshake(ServerHttpRequest request,ServerHttpResponse response, WebSocketHandler wsHandler,
+	      Map<String, Object> attributes) throws Exception {
+	    System.out.println("开始拦截类");
+	    return super.beforeHandshake(request, response, wsHandler, attributes);
+	  }
+
+	  @Override
+	  public void afterHandshake(ServerHttpRequest request,ServerHttpResponse response, WebSocketHandler wsHandler,
+	      Exception ex) {
+	    System.out.println("结束拦截类");
+	    super.afterHandshake(request, response, wsHandler, ex);
+	  }
+
+	}
